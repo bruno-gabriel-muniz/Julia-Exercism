948d7a9ca7922f74bc48230c9772cf0185436fa8
Bruno Gabriel <brunomunizlourenco@gmail.com>
2023-11-06 16:33:23 -0300

    O programa passou em todos os testes do exercício e a parte de código em si já foi concluida, mas ainda falta a documentação

--- leap/leap.jl
+++ leap/leap.jl
@@ -5,6 +5,14 @@ Return `true` if `year` is a leap year in the gregorian calendar.
 
 """
 function is_leap_year(year)
-
+    if year % 4 == 0
+        if year % 100 == 0
+            if year % 400 == 0
+                return true
+            end
+            return false
+        end
+        return true
+    end
+    return false
 end
-
