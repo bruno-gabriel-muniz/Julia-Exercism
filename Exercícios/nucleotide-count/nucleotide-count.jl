789a28ca1552585b4a14d27c290771797c95d015
Bruno Gabriel <brunomunizlourenco@gmail.com>
2023-11-08 09:54:50 -0300

    Exercício totalmente resolvido, mas troquei totalmente a solução. Visto que, agora o algoritimo passa pela lista inteira somando os nucleotideos e quando encontra algo errado ela chama o erro. Porém, ao subir pro exercism da um erro de timeout, que o própio site diz que é devido a infraextrutura deles.

--- Exercícios/nucleotide-count/nucleotide-count.jl
+++ Exercícios/nucleotide-count/nucleotide-count.jl
@@ -7,15 +7,29 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-    if verificador strand
-        DictNucleotideos = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
-        for (NucleotideosName, NumNucleotideos) in DictNucleotideos
-            DictNucleotideos[NucleotideosName] = count(NucleotideosName, uppercase(strand))
+    # Criamos um dicionário que ira conter
+    # o nome e as quantidades de nucleutideos
+    DictNucleotideos = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+    # passamos por todas as letras da entrada
+    for letra in strand
+        # verificamos qual nucleotideo é
+        if letra == 'A'
+            # e adicionamos ao correto
+            DictNucleotideos['A'] += 1
+        elseif letra == 'C'
+            DictNucleotideos['C'] += 1
+        elseif letra == 'G'
+            DictNucleotideos['G'] += 1
+        elseif letra == 'T'
+            DictNucleotideos['T'] += 1
+        # caso não seja nenhum tipo de nucleotídeo
+        else
+            # retornamos o erro de entrada
+            return throw(DomainError(1,"O Valor $letra é Invalido"))
         end
-        return DictNucleotideos
     end
-    return DomainError
+    # e por fim retornamos retornamos os valores encontrados
+    # caso não tenha ocorrido nenhum erro
+    return DictNucleotideos
 end
-function verificador()
-    return true
-end
+
