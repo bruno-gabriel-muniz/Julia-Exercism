567b58c33a9aa8e28c086cf2334b32649ded18ad
Bruno Gabriel <brunomunizlourenco@gmail.com>
2023-11-08 08:40:49 -0300

    A parte que calcula a quantidade de nucleotídeos foi feita. Mas, ainda falta comenta-la e fazer a parte que verifica se a entrada é válida.

--- Exercícios/nucleotide-count/nucleotide-count.jl
+++ Exercícios/nucleotide-count/nucleotide-count.jl
@@ -7,5 +7,15 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-
+    if verificador strand
+        DictNucleotideos = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+        for (NucleotideosName, NumNucleotideos) in DictNucleotideos
+            DictNucleotideos[NucleotideosName] = count(NucleotideosName, uppercase(strand))
+        end
+        return DictNucleotideos
+    end
+    return DomainError
+end
+function verificador()
+    return true
 end
