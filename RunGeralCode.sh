d5e12f290daeb94fb8ae3ba4687a0607ca47d483
bruno-gabriel-muniz <122698842+bruno-gabriel-muniz@users.noreply.github.com>
2024-04-01 20:07:57 +0000

    O arquivo que roda todos os testes foi criado e, além disso, o gitignore foi atualizado

---
+++ RunGeralCode.sh
@@ -0,0 +1,6 @@
+julia	Exercicios/hello-world/runtests.jl &&
+julia	Exercicios/killer-sudoku-helper/runtests.jl &&
+julia Exercicios/leap/runtests.jl &&
+julia Exercicios/nucleotide-count/runtests.jl &&
+julia Exercicios/pangram/runtests.jl &&
+julia Exercicios/rotational-cipher/runtests.jl
